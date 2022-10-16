ac9fba3e860d9cbd92f47912396940279f0f1050
femi <alloolorunfemi@gmail.com>
2022-10-16 22:35:27 +0100

    added the files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
+import Header from './component/header';
 
 const App = () => {
 
 
   return (
     <div>
-      react app starting
+      <Header/>
     </div>
   );
 };
---
+++ src/component/header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return ( 
+        <header className="header">
+            <nav>
+                <div className="nav-container">
+                    <h4>Dashboard</h4>
+                    <div className="nav-link">
+                        <img src="./images/vector.png" alt="" />
+                        <p>iben_Rodi</p>
+                    </div>
+                </div>
+            </nav>
+        </header>
+     );
+}
+ 
+export default Header;
